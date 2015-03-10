7d281c1f1b143d6e70167d7bf6f2d64ed0efe3db
Jeff Squyres <jeff@squyres.com>
2015-03-10 06:48:22 -0400

    battery.c: re-org the battery code a bit
    Rename some variables to make them more clear, ensure to zero out the
    string when we need to, etc.

--- src/battery.c
+++ src/battery.c
@@ -7,30 +7,37 @@
 
 
 void sorta_battery_handler(BatteryChargeState charge) {
-    static char charge_str[8];
-    charge_str[sizeof(charge_str) - 1] = '\0';
+    static char battery_percentage_str[8];
+    battery_percentage_str[sizeof(battery_percentage_str) - 1] = '\0';
 
-    // Only draw something if there is a change
+    // Minimize battery usage: only draw something if there was a
+    // change:
+    // 1. Percentage changed
+    // 2. Plugged-in status changed
     static BatteryChargeState last_charge = { 0 };
-    bool changed = false;
+
+    bool need_to_draw = false;
     if (last_charge.charge_percent != charge.charge_percent ||
         last_charge.is_plugged != charge.is_plugged) {
-        changed = true;
+        need_to_draw = true;
     }
     last_charge = charge;
-    if (!changed) {
+    if (!need_to_draw) {
         return;
     }
 
-    // Only display the battery if it is <= 30% or if it is plugged in
-    if (charge.charge_percent <= 30 ||
-        charge.is_plugged) {
-        snprintf(charge_str, sizeof(charge_str) - 1, "%2d%%",
+    // Only display the battery value if it is <= 30% or if it is
+    // plugged in
+    if (charge.charge_percent <= 30 || charge.is_plugged) {
+        snprintf(battery_percentage_str,
+                 sizeof(battery_percentage_str) - 1, "%2d%%",
                  charge.charge_percent);
-
-        // JMS would be good to print a charging icon if
-        // charge.is_charging or charge.is_plugged is true.
+    } else {
+        battery_percentage_str[0] = '\0';
     }
 
-    text_layer_set_text(s_battery_layer, charge_str);
+    // JMS would be good to print a charging icon if
+    // charge.is_charging or charge.is_plugged is true.
+
+    text_layer_set_text(s_battery_layer, battery_percentage_str);
 }
